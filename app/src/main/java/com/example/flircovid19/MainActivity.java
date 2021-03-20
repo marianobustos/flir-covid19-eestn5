36cfe4252044bd2e9a02ff4ac330bdc6e53ea974
Angel Vazquez <angel.vazquez@extendeal.com>
2021-03-20 16:09:48 -0300

    add mlkit

--- app/src/main/java/com/example/flircovid19/MainActivity.java
+++ app/src/main/java/com/example/flircovid19/MainActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
 
 public class MainActivity extends AppCompatActivity {
-    //Flir
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // High-accuracy landmark detection and face classification
+        FaceDetectorOptions highAccuracyOpts =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .build();
+
+        FaceDetector detector= FaceDetection.getClient(highAccuracyOpts);
     }
 }
